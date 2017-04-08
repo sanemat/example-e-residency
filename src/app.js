7ac7f538eb6ceb5f105368678125a30016525af7
Sanemat <o.gata.ken@gmail.com>
2017-04-08 23:52:28 +0900

    (chore): Show debug message

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-alert("w0000000t");
+function log_text(s) {
+  var d = document.createElement("div");
+  d.innerHTML = s;
+  document.getElementById('log').appendChild(d);
+}
+function debug() {
+  window.hwcrypto.debug()
+    .then(function(response) {
+      log_text("Debug: " + response);
+    });
+}
+document.addEventListener("DOMContentLoaded", function(){
+  debug();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+const BowerResolvePlugin = require("bower-resolve-webpack-plugin");
+
 module.exports = {
+  resolve: {
+    plugins: [new BowerResolvePlugin()],
+    modules: [
+      'bower_components'
+    ],
+    descriptionFiles: ['bower.json', 'package.json'],
+    mainFields: ['main', 'browser']
+  },
   entry: './src/app.js',
   output: {
     filename: 'bundle.js'
